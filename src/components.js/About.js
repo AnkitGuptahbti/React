8ef23117dd2bd698632e3798b19260f6a990c02b
Ankit Gupta <ankitgupta92368892@gmail.com>
2024-06-12 00:06:26 +0530

    class based comp and lifecycle

--- src/components.js/About.js
+++ src/components.js/About.js
@@ -1,7 +1,27 @@
-import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-  return <div>About</div>;
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
 
+    //console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    //console.log("Parent Component Did Mount");
+  }
+
+  render() {
+    //console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass name={"First"} location={"kanpur"} />
+      </div>
+    );
+  }
+}
 export default About;
