3b2d23ecd36380946e2b1c2c69a43c9f682aae16
Ankit Gupta <ankitgupta92368892@gmail.com>
2024-06-15 17:39:38 +0530

    simple accordion only

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -3,9 +3,11 @@ import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 import ItemList from "./ItemList";
+import { useState } from "react";
 // import { useDispatch } from "react-redux";
 
 const RestaurantMenu = () => {
+  const [clicked, setClicked] = useState(true);
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
@@ -21,19 +23,29 @@ const RestaurantMenu = () => {
   // console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR);
   // console.log(itemCards);
 
+  const aquition = () => {
+    setClicked(!clicked);
+  };
+
   return (
     <div className="menu  m-auto  ">
       <h1 className=" text-2xl">{name}</h1>
       <p className="">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      <h2 className="text-xl"> Recommended Menu ⏬</h2>
+      <h2 className="text-xl">
+        Recommended Menu <button onClick={aquition}>⏬</button>
+      </h2>
       <ul className=" rec-menu-con">
-        {itemCards.map((item) => (
-          <div key={item.card.info.id} className="border-4 m-0.5 p-4 ">
-            <ItemList item={item} />
+        {clicked && (
+          <div>
+            {itemCards.map((item) => (
+              <div key={item.card.info.id} className="border-4 m-0.5 p-4 ">
+                <ItemList item={item} />
+              </div>
+            ))}
           </div>
-        ))}
+        )}
       </ul>
     </div>
   );
